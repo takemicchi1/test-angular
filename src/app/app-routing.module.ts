7d1977a2875d829c3e884328105ae669270d5e6e
hoangbru <hoangbru07@gmail.com>
2023-06-16 02:00:47 +0700

    style: base layout web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AdminProductListComponent } from './pages/admin/admin-product-list/admin-product-list.component';
+import { AdminProductAddComponent } from './pages/admin/admin-product-add/admin-product-add.component';
+import { AdminProductEditComponent } from './pages/admin/admin-product-edit/admin-product-edit.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: BaseLayoutComponent},
+  {path: "admin", component: AdminLayoutComponent, children:[
+    {path: "", redirectTo:"dashboard", pathMatch:"full"},
+    {path: "dashboard", component: DashboardComponent},
+    {path: "products", component: AdminProductListComponent},
+    {path: "product/add", component: AdminProductAddComponent},
+    {path: "product/:id/edit", component: AdminProductEditComponent},
+  ]},
+  {path: "signin", component: SigninComponent},
+  {path: "signup", component: SignupComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { AdminProductListComponent } from './pages/admin/admin-product-list/admin-product-list.component';
+import { AdminProductAddComponent } from './pages/admin/admin-product-add/admin-product-add.component';
+import { AdminProductEditComponent } from './pages/admin/admin-product-edit/admin-product-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BaseLayoutComponent,
-    AdminLayoutComponent
+    AdminLayoutComponent,
+    DashboardComponent,
+    SigninComponent,
+    SignupComponent,
+    AdminProductListComponent,
+    AdminProductAddComponent,
+    AdminProductEditComponent,
   ],
   imports: [
     BrowserModule,
